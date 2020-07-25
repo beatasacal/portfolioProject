537dc4028c473fa0934f4f6d6a8d6e95655cf581
krisztina.sacal <krisztina.sacal@fortech.ro>
2020-07-25 20:57:35 +0300

    Adjusting home page, separating some components and creating resume page

--- personalPortfolio/src/app/app.module.ts
+++ personalPortfolio/src/app/app.module.ts
@@ -3,16 +3,29 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomePageComponent } from './home-page/home-page.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { ResumeComponent } from './components/resume/resume.component';
+import { DetailsComponent } from './components/details/details.component';
+import { RouterModule } from '@angular/router';
+import { LogoInfoComponent } from './components/logo-info/logo-info.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomePageComponent
+    HomePageComponent,
+    ResumeComponent,
+    DetailsComponent,
+    LogoInfoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: 'home-page', component: HomePageComponent },
+      { path: 'resume', component: ResumeComponent },
+      { path: '', redirectTo: 'home-page', pathMatch: 'full' }
+    ], { useHash: true }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
